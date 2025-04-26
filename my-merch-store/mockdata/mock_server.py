cbcf011bb5ffd0628ef3ea29a76636fc022b6835
Drew-chen-yc <147859780+Drew-chen-yc@users.noreply.github.com>
2025-04-25 20:37:01 -0500

    Update mock_server.py

--- my-merch-store/mockdata/mock_server.py
+++ my-merch-store/mockdata/mock_server.py
@@ -2,14 +2,14 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Allowing CORS (for cross container exercution)
+CORS(app)  # Allowing CORS (for cross container execution)
 
 @app.route('/items')
 def items():
     return jsonify([
-        {"name": "T-Shirt", "price": 20},
-        {"name": "Hat", "price": 10},
-        {"name": "Sticker", "price": 2}
+        {"name": "T-Shirt", "price": 20, "image": "http://localhost:5001/static/images/p1.png"}, 
+        {"name": "Hat", "price": 10, "image": "http://localhost:5001/static/images/p2.png"}, 
+        {"name": "Sticker", "price": 2, "image": "http://localhost:5001/static/images/p3.png"}
     ])
 
 if __name__ == '__main__':
