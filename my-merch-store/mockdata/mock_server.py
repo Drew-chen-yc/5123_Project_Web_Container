bd5a8b88e9015630e54bda900582482bff929986
Keevin Funderburg <KEEVINFUNDERBURG@boisestate.edu>
2025-04-30 13:44:08 -0500

    reviews added to 5001/reviews

--- my-merch-store/mockdata/mock_server.py
+++ my-merch-store/mockdata/mock_server.py
@@ -12,7 +12,7 @@ user = 'storefront'
 password = '12345'
 host = 'db'
 port = 5432
-database = 'MerchDB'
+database = 'GamezoneDB'
 
 # Configure PostgreSQL database
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://{0}:{1}@{2}:{3}/{4}".format(
@@ -32,6 +32,16 @@ class Merch(db.Model):
     def __repr__(self) -> str:
         return f"Merch(name={self.name!r}, price={self.price!r})"
 
+class Reviews(db.Model):
+    __tablename__ = 'reviews'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    rating: Mapped[int]
+    review: Mapped[str]
+
+    def __repr__(self) -> str:
+        return f"Reviews(title={self.title!r}, rating={self.rating!r})"
+
 @app.route('/items')
 def items():
     merch = db.session.execute(db.select(Merch)).scalars().all()
@@ -48,6 +58,21 @@ def items():
 
     return jsonify(merch_list)
 
+@app.route('/reviews')
+def reviews():
+    game_reviews = db.session.execute(db.select(Reviews)).scalars().all()
+
+    # Convert the list of reviews objects into dictionaries
+    reviews_list = []
+    for review in game_reviews:
+        reviews_list.append({
+            'id': review.id,
+            'title': review.title,
+            'rating': review.rating,
+            'review': review.review
+        })
+
+    return jsonify(reviews_list)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
